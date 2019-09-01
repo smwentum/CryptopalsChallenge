f8c4c7d3fbab35b14694eb20fc03cb305b36155e
sai wentum <michael.wentum@gmail.com>
2019-09-01 15:05:54 -0400

    Add project files.

---
+++ CryptopalsChallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CryptopalsChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
